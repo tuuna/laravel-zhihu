96286b9744377d0b0301e345e2e511df2ca70b33
vampirebitter <lj550566181@gmail.com>
2017-01-30 22:06:57 +0800

    init

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -8,6 +8,16 @@ class EmailController extends Controller
 {
     public function verify()
     {
+        Mail::send('随便传个空view', [], function ($message) {
+            $message->from('us@example.com', 'Laravel');
 
+            $message->to('foo@example.com')->cc('bar@example.com');
+
+            // 模板变量
+            $bind_data = ['url' => 'http://naux.me'];
+            $template = new SendCloudTemplate('模板名', $bind_data);
+
+            $message->getSwiftMessage()->setBody($template);
+        });
     }
 }
