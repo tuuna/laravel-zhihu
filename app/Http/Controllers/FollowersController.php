215292e868641b07c3e68b7892e897234cf4f9f5
vampirebitter <lj550566181@gmail.com>
2017-02-08 21:53:11 +0800

    update

--- app/Http/Controllers/FollowersController.php
+++ app/Http/Controllers/FollowersController.php
@@ -23,7 +23,7 @@ class FollowersController extends Controller
     public function index(Request $request)
     {
         $user = $this->user->byId($request->get('user'));
-        $followers = $user->followers()->pluck('follower_id')->toArray();
+        $followers = $user->followersUser()->pluck('follower_id')->toArray();
 
         if(in_array(Auth::guard('api')->user()->id,$followers)) {
             return response()->json(['followed' => true]);
@@ -34,6 +34,16 @@ class FollowersController extends Controller
 
     public function follow()
     {
+        $userToFollow = $this->user->byId(request('user'));
 
+        $followed = Auth::guard('api')->user()->followThisUser($userToFollow->id);
+
+        if (count($followed['attached']) > 0) {
+            $userToFollow->increment('followers_count');
+
+            return response()->json(['followed' => true]);
+        }
+        $userToFollow->decrement('followers_count');
+        return response()->json(['followed' => false]);
     }
 }
