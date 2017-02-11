0d413be9f2f3f1b504d50557655f580bce92f353
vampirebitter <lj550566181@gmail.com>
2017-02-11 15:19:51 +0800

    update

--- app/Comment.php
+++ app/Comment.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    protected $table = 'comments';
+
+    protected $fillable = ['user_id','body','commentable_id','commentable_type'];
+
+    public function commentable()
+    {
+        return $this->morphTo();
+    }
 }
